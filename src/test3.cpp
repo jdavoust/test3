1f692bd908de3d4a8610211301167c81b93732a6
jean.davoust <davoustj@gmail.com>
2014-03-19 21:43:35 +0100

    test git eclipse
    Signed-off-by: jean.davoust <davoustj@gmail.com>

---
+++ src/test3.cpp
@@ -0,0 +1,19 @@
+//============================================================================
+// Name        : test3.cpp
+// Author      : jean
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	for (int i = 0; i < 10; i++)
+	{
+		cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	}
+	cout << "Etçacontinue!" << endl;
+	return 0;
+}
